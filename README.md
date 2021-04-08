3cf44cdf9d248cdbe8f1b8da7db51b129e5c45ec
wojiushixiaobai <296015668@qq.com>
2021-04-08 12:21:28 +0800

    init: 更新代码

--- README.md
+++ README.md
@@ -1,1 +1,101 @@
 # openvpn
+
+## 支持的操作系统
+
+- [x] CentOS 7
+- [ ] CentOS 8
+
+## 安装方法
+
+- 安装完成后, 客户端所需的文件在 /etc/openvpn/client/ 目录里面
+
+```bash
+git clone --depth=1 https://github.com/wojiushixiaobai/openvpn
+cd openvpn
+```
+
+### 安装
+
+```bash
+./install.sh install
+```
+
+### 其他
+
+```vim
+# Linux 客户端使用方式
+openvpn --daemon --cd /etc/openvpn --config client.ovpn
+```
+
+```vim
+# Windows 客户端使用方式
+client.ovpn
+
+ca [inline]
+cert [inline]
+key [inline]
+
+<ca>
+...
+</ca>
+
+<cert>
+ ...
+</cert>
+
+<key>
+...
+</key>
+```
+
+```vim
+# 使用密码认证
+vi /etc/openvpn/server.conf
+
+# Password Authentication
+script-security 3
+auth-user-pass-verify /etc/openvpn/checkpsw.sh via-env
+client-cert-not-required
+username-as-common-name
+
+```
+```vim
+# 密码校验脚本
+ vi /etc/openvpn/checkpsw.sh
+
+# Checkout Password
+#!/bin/sh
+PASSFILE="/etc/openvpn/psw-file"
+LOG_FILE="/etc/openvpn/openvpn-password.log"
+TIME_STAMP=`date "+%Y-%m-%d %T"`
+if [ ! -r "${PASSFILE}" ]; then
+  echo "${TIME_STAMP}: Could not open password file \"${PASSFILE}\" for reading." >> ${LOG_FILE}
+  exit 1
+fi
+CORRECT_PASSWORD=`awk '!/^;/&&!/^#/&&$1=="'${username}'"{print $2;exit}' ${PASSFILE}`
+if [ "${CORRECT_PASSWORD}" = "" ]; then
+  echo "${TIME_STAMP}: User does not exist: username=\"${username}\", password=\"${password}\"." >> ${LOG_FILE}
+  exit 1
+fi
+if [ "${password}" = "${CORRECT_PASSWORD}" ]; then
+  echo "${TIME_STAMP}: Successful authentication: username=\"${username}\"." >> ${LOG_FILE}
+  exit 0
+fi
+echo "${TIME_STAMP}: Incorrect password: username=\"${username}\", password=\"${password}\"." >> ${LOG_FILE}
+exit 1
+```
+
+```vim
+# 密码文件
+vi /etc/openvpn/psw-file
+
+# user password
+test 123456
+```
+
+```vim
+# 客户端使用密码认证
+client.ovpn
+
+auth-user-pass
+```
---
+++ install.sh
@@ -0,0 +1,221 @@
+#!/bin/bash
+#
+
+set -e
+
+BASE_DIR=$(cd "$(dirname "$0")";pwd)
+PROJECT_DIR=${BASE_DIR}
+
+if [ ! "$(rpm -qa | grep openvpn)" ]; then
+  yum install -y openvpn
+  systemctl enable openvpn@server
+fi
+if [ ! "$(rpm -qa | grep easy-rsa)" ]; then
+  yum install -y easy-rsa
+fi
+
+if [ ! -d "/etc/openvpn/easy-rsa/3" ]; then
+  cp -R /usr/share/easy-rsa/3 /etc/openvpn/easy-rsa/
+fi
+
+cd /etc/openvpn/easy-rsa/3
+
+if [ ! -f "/etc/openvpn/easy-rsa/3/vars" ]; then
+  find / -type f -name "vars.example" | xargs -i cp {} . && mv vars.example vars
+fi
+
+if [ ! -d "/etc/openvpn/easy-rsa/3/pki" ]; then
+  ./easyrsa init-pki && sleep 1s
+fi
+
+if [ ! -f "/etc/openvpn/easy-rsa/3/pki/ca.crt" ]; then
+  echo | ./easyrsa build-ca nopass && sleep 1s
+fi
+
+if [ ! -f "/etc/openvpn/easy-rsa/3/pki/reqs/server.req" ]; then
+  echo | ./easyrsa gen-req server nopass && sleep 1s
+fi
+
+if [ ! -f "/etc/openvpn/easy-rsa/3/pki/issued/server.crt" ]; then
+  echo "yes" | ./easyrsa sign server server && sleep 1s
+fi
+
+if [ ! -f "/etc/openvpn/easy-rsa/3/pki/dh.pem" ]; then
+  ./easyrsa gen-dh && sleep 1s
+fi
+
+if [ ! -d "/etc/openvpn/client/easy-rsa" ]; then
+  mkdir -p /etc/openvpn/client/easy-rsa
+fi
+
+if [ ! -d "/etc/openvpn/client/easy-rsa/3" ]; then
+  cp -R /usr/share/easy-rsa/3 /etc/openvpn/client/easy-rsa
+fi
+
+cd /etc/openvpn/client/easy-rsa/3
+
+if [ ! -f "/etc/openvpn/client/easy-rsa/3/vars" ]; then
+  find / -type f -name "vars.example" | xargs -i cp {} . && mv vars.example vars
+fi
+
+if [ ! -d "/etc/openvpn/client/easy-rsa/3/pki" ]; then
+ ./easyrsa init-pki && sleep 1s
+fi
+
+if [ ! -f "/etc/openvpn/client/3/pki/reqs/client.req" ]; then
+  echo | ./easyrsa gen-req client nopass && sleep 1s
+fi
+
+cd /etc/openvpn/easy-rsa/3
+./easyrsa import-req /etc/openvpn/client/easy-rsa/3/pki/reqs/client.req client && echo "yes" | ./easyrsa sign client client && sleep 1s
+
+if [ ! -d "/etc/openvpn/certs" ]; then
+  mkdir /etc/openvpn/certs
+fi
+
+if [ ! -f "/etc/openvpn/certs/dh.pem" ]; then
+  cp /etc/openvpn/easy-rsa/3/pki/dh.pem /etc/openvpn/certs/
+fi
+
+if [ ! -f "/etc/openvpn/certs/ca.crt" ]; then
+  cp /etc/openvpn/easy-rsa/3/pki/ca.crt /etc/openvpn/certs/
+fi
+
+if [ ! -f "/etc/openvpn/certs/server.crt" ]; then
+  cp /etc/openvpn/easy-rsa/3/pki/issued/server.crt /etc/openvpn/certs/
+fi
+
+if [ ! -f "/etc/openvpn/certs/server.key" ]; then
+  cp /etc/openvpn/easy-rsa/3/pki/private/server.key /etc/openvpn/certs/
+fi
+
+if [ ! -f "/etc/openvpn/client/ca.crt" ]; then
+  cp /etc/openvpn/easy-rsa/3/pki/ca.crt /etc/openvpn/client/
+fi
+
+if [ ! -f "/etc/openvpn/client/client.crt" ]; then
+  cp /etc/openvpn/easy-rsa/3/pki/issued/client.crt /etc/openvpn/client/
+fi
+
+if [ ! -f "/etc/openvpn/client/client.key" ]; then
+  cp /etc/openvpn/client/easy-rsa/3/pki/private/client.key /etc/openvpn/client/
+fi
+
+if [ "$(firewall-cmd --state)" == "running" ]; then
+  if [ ! "$(firewall-cmd --list-services | grep openvpn)" ]; then
+    firewall-cmd --permanent --add-masquerade
+    firewall-cmd --permanent --zone=public --add-service=openvpn
+    firewalld_flag=1
+  fi
+  if [ "$firewalld_flag" ]; then
+    firewall-cmd --reload
+  fi
+else
+  if [ ! "$(iptables -L -n -t nat | grep 10.8.0.0/24)" ]; then
+    iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -j MASQUERADE
+  fi
+fi
+
+if ! grep -q "net.ipv4.ip_forward" /etc/sysctl.conf; then
+  echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+  sysctl_flag=1
+fi
+if [ "$sysctl_flag" ]; then
+  sysctl -p
+fi
+
+if [ ! -f "/etc/openvpn/server.conf" ]; then
+  cat > /etc/openvpn/server.conf << "EOF"
+local 0.0.0.0
+port 1194
+;proto tcp
+proto udp
+;dev tap
+dev tun
+;dev-node MyTap
+ca /etc/openvpn/certs/ca.crt
+cert /etc/openvpn/certs/server.crt
+key /etc/openvpn/certs/server.key  # This file should be kept secret
+dh /etc/openvpn/certs/dh.pem
+
+;topology subnet
+
+server 10.8.0.0 255.255.255.0
+ifconfig-pool-persist /etc/openvpn/ipp.txt
+
+;server-bridge 10.8.0.4 255.255.255.0 10.8.0.50 10.8.0.100
+;server-bridge
+;push "route 192.168.10.0 255.255.255.0"
+;push "route 192.168.20.0 255.255.255.0"
+;client-config-dir ccd
+;route 192.168.40.128 255.255.255.248
+;client-config-dir ccd
+;route 10.9.0.0 255.255.255.252
+;learn-address ./script
+
+push "redirect-gateway def1 bypass-dhcp"
+
+;push "dhcp-option DNS 208.67.222.222"
+;push "dhcp-option DNS 208.67.220.220"
+
+push "dhcp-option DNS 192.168.100.1"
+client-to-client
+
+;duplicate-cn
+
+keepalive 10 120
+cipher AES-256-CBC
+
+;compress lz4-v2
+;push "compress lz4-v2"
+
+comp-lzo
+
+;max-clients 100
+;user nobody
+;group nobody
+
+persist-key
+persist-tun
+status openvpn-status.log
+
+;log         openvpn.log
+
+log-append  openvpn.log
+verb 3
+mute 20
+explicit-exit-notify 1
+EOF
+fi
+
+if [ ! -f "/etc/openvpn/client/client.ovpn" ]; then
+  cat > /etc/openvpn/client/client.ovpn << "EOF"
+client
+dev tun
+proto udp
+remote <server> 1194
+ca /etc/openvpn/client/ca.crt
+cert /etc/openvpn/client/client.crt
+key /etc/openvpn/client/client.key
+
+resolv-retry infinite
+nobind
+mute-replay-warnings
+
+keepalive 20 120
+comp-lzo
+
+persist-key
+persist-tun
+status openvpn-status.log
+log-append openvpn.log
+verb 3
+mute 20
+EOF
+fi
+
+if [ ! "$(systemctl status openvpn@server | grep Active | grep running)" ]; then
+  systemctl start openvpn@server
+fi
+
+exit 0
